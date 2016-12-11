b6153b038e5dce494a17a1cabd6fb06916076d88
banojov <jovana.banovic@uoit.net>
2016-12-11 07:42:54 -0500

    Add files via upload
    Print Board

--- TicTacToe.rb
+++ TicTacToe.rb
@@ -1,12 +1,25 @@
 class Board
 
 	def initialize
-		@board= Array.new(3)
-		{Array.new(3," ")}
+		@board= Array.new(3) { Array.new(3," ") }
 	end
 
-	def printframe
+	def defineframe
 		puts "1 | 2 | 3 |",
 			 "4 | 5 | 6 |",
 			 "7 | 8 | 9 |"
 		print "\n"
+	end
+
+	def printboard
+		(0..3).each do |row|
+			print "       "
+		(0..3).each do |col|
+			print @defineframe[row][col]
+			print " | " unless col == 2
+		end
+		print "\n"
+	end
+		print "\n"
+	end
+	
